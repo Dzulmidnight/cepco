0f432ac5a7fa6daad82205da50827e9bcf6a1ade
JMH <isc.jesusmartinez@gmail.com>
2016-09-13 20:41:33 -0500

    vinculo inicio

--- section0.php
+++ section0.php
@@ -4,35 +4,35 @@
           <div class="caption">
             <h1 class="animated fadeInLeftBig"><span>CEPCO</span></h1>
             <p class="animated fadeInRightBig">Coordinadora Estatal de Productores de Café de Oaxaca</p>
-            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#services">Saber Más</a>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#about-us">Saber Más</a>
           </div>
         </div>
         <div class="item" style="background-image: url(images/slider/2.jpg)">
           <div class="caption">
             <h1 class="animated fadeInLeftBig"><span>CEPCO</span></h1>
             <p class="animated fadeInRightBig">Bootstrap - Responsive Design - Retina Ready - Parallax</p>
-            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#services">Start now</a>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#about-us">Start now</a>
           </div>
         </div>
         <div class="item" style="background-image: url(images/slider/3.jpg)">
           <div class="caption">
             <h1 class="animated fadeInLeftBig">We are <span>CEPCO</span></h1>
             <p class="animated fadeInRightBig">Bootstrap - Responsive Design - Retina Ready - Parallax</p>
-            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#services">Start now</a>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#about-us">Start now</a>
           </div>
         </div>
         <div class="item" style="background-image: url(images/slider/4.jpg)">
           <div class="caption">
             <h1 class="animated fadeInLeftBig">We are <span>Coffee Producers</span></h1>
             <p class="animated fadeInRightBig">Bootstrap - Responsive Design - Retina Ready - Parallax</p>
-            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#services">Start now</a>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#about-us">Start now</a>
           </div>
         </div>
         <div class="item" style="background-image: url(images/slider/5.jpg)">
           <div class="caption">
             <h1 class="animated fadeInLeftBig">We are <span>Creative</span></h1>
             <p class="animated fadeInRightBig">Bootstrap - Responsive Design - Retina Ready - Parallax</p>
-            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#services">Start now</a>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#about-us">Start now</a>
           </div>
         </div>
 
@@ -40,6 +40,6 @@
       <a class="left-control" href="#home-slider" data-slide="prev"><i class="fa fa-angle-left"></i></a>
       <a class="right-control" href="#home-slider" data-slide="next"><i class="fa fa-angle-right"></i></a>
 
-      <a id="tohash" href="#services"><i class="fa fa-angle-down"></i></a>
+      <a id="tohash" href="#about-us"><i class="fa fa-angle-down"></i></a>
 
     </div><!--/#home-slider-->
